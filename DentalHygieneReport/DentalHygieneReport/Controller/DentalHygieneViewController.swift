45bbbed40d011ece28a41169ad258e490e6359f4
Avinash Reddy <beavinash15@gmail.com>
2020-03-01 07:44:58 -0800

    Dental Hygiene Report Starter

---
+++ DentalHygieneReport/DentalHygieneReport/Controller/DentalHygieneViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DentalHygieneViewController.swift
+//  DentalHygieneReport
+//
+//  Created by Avinash Reddy on 3/1/20.
+//  Copyright © 2020 Avinash Reddy. All rights reserved.
+//
+
+import UIKit
+
+class DentalHygieneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
